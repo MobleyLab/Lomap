52c9527660d8703168a61a766d7dcc3bc33ed886
nividic <gcalabro@uci.edu>
2016-02-04 14:03:17 -0800

    Add Licence and authors

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+#############################################################################
+# Lomap2: A toolkit to plan alchemical relative binding affinity calculations
+# Copyright 2015 - 2016  UC Irvine and the Authors
+#
+# Authors: Dr Gaetano Calabro' and Dr David Mobley
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see http://www.gnu.org/licenses/
+#############################################################################
+
 """Main program
 """
 
@@ -36,9 +56,6 @@ def build_database(molid_list, options) :
     db_mol.build_matrices()
     db_mol.build_graph()
     
-    # print db_mol.strict_mtx
-    # print db_mol.loose_mtx
-
 
 def startup() :
     """
