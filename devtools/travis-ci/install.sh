9461f8d3ea94a8a14570ce1fcc380d49f181829c
nividic <gcalabro@uci.edu>
2016-05-06 10:11:25 -0700

    travis fix

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -1,27 +1,14 @@
-# Install Miniconda
 MINICONDA=Miniconda2-latest-Linux-x86_64.sh
-MINICONDA_HOME=$HOME/miniconda
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget -q http://repo.continuum.io/miniconda/$MINICONDA
+wget http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
-bash $MINICONDA -b -p $MINICONDA_HOME
+bash $MINICONDA -b -p $HOME/miniconda
+PIP_ARGS="-U"
 
-# Configure miniconda
-export PIP_ARGS="-U"
-export PATH=$MINICONDA_HOME/bin:$PATH
+export PATH=$HOME/miniconda/bin:$PATH
 
-conda update --yes conda
+conda install --yes conda-build jinja2 anaconda-client pip
 
-conda config --add channels omnia
-
-conda install --yes conda-build
-
-echo "HERE>>>>>>>>>>>>>>>>>>>>>>>>"
-
-conda build devtools/conda-recipe
-
-conda create -n myenv --use-local conda-recipe
-source activate myenv
