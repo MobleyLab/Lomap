e9cf562ee2bfd8a274fbaa9835c4ff57dc2d8bff
Gaetano Calabró <gac@andromeda.local>
2017-04-16 00:14:38 -0600

    Restore the conda env as it was

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -10,5 +10,5 @@ PIP_ARGS="-U"
 
 export PATH=$HOME/miniconda/bin:$PATH
 
-conda install --yes conda-build=2.1.9 jinja2 anaconda-client pip 
+conda install --yes conda-build jinja2 anaconda-client pip 
 
