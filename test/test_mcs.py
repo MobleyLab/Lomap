211965a533bf1942b37a3b7546f60f3ba7177c44
richard <richard@nextmovesoftware.com>
2022-02-10 14:03:51 +0000

    use fixture of fixtures to avoid repetition

--- test/test_mcs.py
+++ test/test_mcs.py
@@ -16,121 +16,87 @@ def _rf(fn):
     return f.replace('/lomap/test', '/test')
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def toluene():
-    f = _rf('basic/toluene.mol2')
-
-    return Chem.MolFromMol2File(f)
+    return Chem.MolFromMol2File(_rf('basic/toluene.mol2'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def methylcyclohexane():
-    f = _rf('basic/methylcyclohexane.mol2')
-
-    return Chem.MolFromMol2File(f)
+    return Chem.MolFromMol2File(_rf('basic/methylcyclohexane.mol2'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def methylnaphthalene():
-    f = _rf('basic/2-methylnaphthalene.mol2')
-
-    return Chem.MolFromMol2File(f)
+    return Chem.MolFromMol2File(_rf('basic/2-methylnaphthalene.mol2'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def trimethylnaphthalene():
-    f = _rf('basic/1,3,7-trimethylnaphthalene.mol2')
-
-    return Chem.MolFromMol2File(f)
+    return Chem.MolFromMol2File(_rf('basic/1,3,7-trimethylnaphthalene.mol2'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def butylmethylbenzene():
-    f = _rf('basic/1-butyl-4-methylbenzene.mol2')
-
-    return Chem.MolFromMol2File(f)
+    return Chem.MolFromMol2File(_rf('basic/1-butyl-4-methylbenzene.mol2'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def dimethylnaphthalene():
-    f = _rf('basic/2,6-dimethylnaphthalene.mol2')
-
-    return Chem.MolFromMol2File(f)
+    return Chem.MolFromMol2File(_rf('basic/2,6-dimethylnaphthalene.mol2'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def methylpropylnaphthalene():
-    f = _rf('basic/2-methyl-6-propylnaphthalene.mol2')
-
-    return Chem.MolFromMol2File(f)
+    return Chem.MolFromMol2File(_rf('basic/2-methyl-6-propylnaphthalene.mol2'))
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def naphthol():
-    f = _rf('basic/2-naftanol.mol2')
-
-    return Chem.MolFromMol2File(f)
+    return Chem.MolFromMol2File(_rf('basic/2-naftanol.mol2'))
 
 
-def test_toluene_to_methylcyclohexane(toluene, methylcyclohexane):
-    mapper = mcs.MCS(toluene, methylcyclohexane)
-
-    mapping = mapper.heavy_atom_mcs_map()
-
-    assert sorted(mapping) == [(0, 0), (1, 1), (2, 6), (3, 5), (4, 4), (5, 3),
-                               (6, 2)]
-
-
-def test_toluene_to_methylnaphthalene(toluene, methylnaphthalene):
-    mapper = mcs.MCS(toluene, methylnaphthalene)
-
-    mapping = mapper.heavy_atom_mcs_map()
-
-    assert len(mapping) == 7
-    assert sorted(mapping) == [(0, 0), (1, 1), (2, 2), (3, 3), (4, 8), (5, 9),
-                               (6, 10)]
-
-
-def test_toluene_to_trimethylnaphthalene(toluene, trimethylnaphthalene):
-    mapper = mcs.MCS(toluene, trimethylnaphthalene)
-
-    mapping = mapper.heavy_atom_mcs_map()
-
-    assert sorted(mapping) == [(0, 0), (1, 1), (2, 10), (3, 9), (4, 8), (5, 3),
-                               (6, 2)]
-
-
-def test_toluene_to_naphthol(toluene, naphthol):
-    mapper = mcs.MCS(toluene, naphthol)
-
-    mapping = mapper.heavy_atom_mcs_map()
-
-    assert sorted(mapping) == [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 9),
-                               (6, 10)]
-
-
-def test_toluene_to_butylmethylbenzene(toluene, butylmethylbenzene):
-    mapper = mcs.MCS(toluene, butylmethylbenzene)
-
-    mapping = mapper.heavy_atom_mcs_map()
-
-    assert sorted(mapping) == [(0, 3), (1, 4), (2, 5), (3, 6), (4, 7), (5, 8),
-                               (6, 9)]
+@pytest.fixture(scope='function', params=[
+    'methylcyclohexane', 'methylnaphthalene', 'trimethylnaphthalene',
+    'butylmethylbenzene', 'dimethylnaphthalene', 'methylpropylnaphthalene',
+    'naphthol',
+])
+def regression_mappings(toluene, methylcyclohexane, methylnaphthalene,
+                        trimethylnaphthalene, butylmethylbenzene,
+                        dimethylnaphthalene, methylpropylnaphthalene,
+                        naphthol, request):
+    regression_mappings = {
+        'methylcyclohexane': (methylcyclohexane,
+                              [(0, 0), (1, 1), (2, 6), (3, 5), (4, 4), (5, 3),
+                               (6, 2)]),
+        'methylnaphthalene':  (methylnaphthalene,
+                               [(0, 0), (1, 1), (2, 2), (3, 3), (4, 8), (5, 9),
+                               (6, 10)]),
+        'trimethylnaphthalene': (trimethylnaphthalene,
+                                 [(0, 0), (1, 1), (2, 10), (3, 9), (4, 8),
+                                  (5, 3), (6, 2)]),
+        'butylmethylbenzene': (butylmethylbenzene,
+                               [(0, 3), (1, 4), (2, 5), (3, 6), (4, 7), (5, 8),
+                                (6, 9)]),
+        'dimethylnaphthalene': (dimethylnaphthalene,
+                                [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4),
+                                 (5, 10), (6, 11)]),
+        'methylpropylnaphthalene': (methylpropylnaphthalene,
+                                    [(0, 2), (1, 3), (2, 8), (3, 7), (4, 6),
+                                     (5, 5), (6, 4)]),
+        'naphthol': (naphthol,
+                     [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 9), (6, 10)])
+    }
+    other, other_mapping = regression_mappings[request.param]
+    # a bit paranoid, but return copies
+    return Chem.Mol(toluene), Chem.Mol(other), other_mapping
 
 
-def test_toluene_dimethylnaphthalene(toluene, dimethylnaphthalene):
-    mapper = mcs.MCS(toluene, dimethylnaphthalene)
+def test_toluene_to_other(regression_mappings):
+    toluene, other, ref_mapping = regression_mappings
+
+    mapper = mcs.MCS(toluene, other)
 
     mapping = mapper.heavy_atom_mcs_map()
 
-    assert sorted(mapping) == [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 10),
-                               (6, 11)]
-
-
-def test_toluene_to_methylpropylnaphthalene(toluene, methylpropylnaphthalene):
-    mapper = mcs.MCS(toluene, methylpropylnaphthalene)
-
-    mapping = mapper.heavy_atom_mcs_map()
-
-    assert sorted(mapping) == [(0, 2), (1, 3), (2, 8), (3, 7), (4, 6), (5, 5),
-                               (6, 4)]
+    assert sorted(mapping) == ref_mapping
